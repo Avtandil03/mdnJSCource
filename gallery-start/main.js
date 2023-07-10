b3f970a1f924d2f4137c0ad361cab587b95a3c2d
Avtandil Saparov <avtanndil@gmail.com>
2023-07-10 13:11:47 +0300

    task done

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -4,15 +4,31 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+const imgFileNames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+const imgAltTexts = [
+  'Closeup of a human eye', 
+  'Rock that looks like a wave',
+  'Purple and white pansies',
+  'Section of wall from a pharoah\'s tomb',
+  'Large moth on a leaf',
+]
 
-/* Declaring the alternative text for each image file */
 
-/* Looping through images */
+for (let i = 0; i < imgFileNames.length; i++) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `./images/${imgFileNames[i]}`);
+  newImage.setAttribute('alt', imgAltTexts[i]);
+  thumbBar.appendChild(newImage);
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+thumbBar.addEventListener('click', (e) => {
+  const target = e.target
+  if(!target) return
+  displayedImage.setAttribute('src', target.getAttribute('src'))
+  displayedImage.setAttribute('alt', target.getAttribute('alt'))
+})
 
-/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  overlay.classList.toggle('dark')
+})
