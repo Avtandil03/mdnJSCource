ee9ee14f595bb8e6a0e0f4406f53aa5ac2a82fe6
Avtandil Saparov <avtanndil@gmail.com>
2023-07-10 21:31:02 +0300

    solution

--- sequensing-animations/mainl.js
+++ sequensing-animations/mainl.js
@@ -9,8 +9,25 @@ const aliceTiming = {
   fill: "forwards",
 };
 
-const alice1 = document.querySelector("#alice1");
-const alice2 = document.querySelector("#alice2");
-const alice3 = document.querySelector("#alice3");
 
-alice1.animate(aliceTumbling, aliceTiming);
+// const alice1 = document.querySelector("#alice1");
+// const alice2 = document.querySelector("#alice2");
+// const alice3 = document.querySelector("#alice3");
+
+// alice1.animate(aliceTumbling, aliceTiming).finished
+// .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+// .then(() => alice3.animate(aliceTumbling, aliceTiming))
+
+const alices = document.querySelector('#alice-container').children
+
+
+const animateAll = async (step = 0) => {
+  if(!alices[step]) return
+  await alices[step].animate(aliceTumbling, aliceTiming).finished
+    .then(() => {
+      step++
+      return animateAll(step)
+    })
+}
+
+animateAll()
